4ec70a932eac3aad0d395a0ea01e360b9f53cc73
gussjos <gussjos@hotmail.com>
2022-10-07 11:43:29 +0200

    Remap

--- mv_windows_on_startup.sh
+++ mv_windows_on_startup.sh
@@ -4,10 +4,8 @@ wmctrl -r Firefox -t 0
 wmctrl -r Teams -t 0 
 wmctrl -r Hyper -t 1
 wmctrl -s 1 
-wmctrl -r Spotify -t 4
 
-wmctrl -r Toggl -t 0
-wmctrl -r Toggl -t 7
+wmctrl -r Spotify -t 6
 
-wmctrl -r Slack -t 0
-wmctrl -r Slack -t 7 
+wmctrl -r Toggl -t 9
+wmctrl -r Slack -t 9
