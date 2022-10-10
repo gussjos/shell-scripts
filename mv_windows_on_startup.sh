0b2008998160f4232fd3989732c36e006602e776
gussjos <gussjos@hotmail.com>
2022-10-10 15:09:56 +0200

    Removed workspace 9-12

--- mv_windows_on_startup.sh
+++ mv_windows_on_startup.sh
@@ -6,7 +6,7 @@ wmctrl -r Hyper -t 1
 wmctrl -s 1 
 
 wmctrl -r Code -t 2
-wmctrl -r Spotify -t 6
+wmctrl -r Spotify -t 5
 
-wmctrl -r Toggl -t 9
-wmctrl -r Slack -t 9
+wmctrl -r Toggl -t 7 
+wmctrl -r Slack -t 7 
